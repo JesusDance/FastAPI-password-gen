6d5ea15ba0c48de546b54cfed1bf77eef9b58273
Jesus <jiesusdance@gmail.com>
2026-04-07 19:12:06 +0300

    Reformatted code using Black

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import random
-import uvicorn
 from typing import Any, Annotated
-from pydantic import BaseModel
 
+import uvicorn
 from fastapi import FastAPI, HTTPException, status, Form, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-
+from pydantic import BaseModel
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -31,8 +30,12 @@ def password_gen():
     random_letters_upper_case = random.sample(letters_upper_case, k=4)
     random_numbers = random.sample(numbers, k=4)
 
-    password = (random_symbols + random_letters_lower_case +
-                       random_letters_upper_case + random_numbers)
+    password = (
+        random_symbols
+        + random_letters_lower_case
+        + random_letters_upper_case
+        + random_numbers
+    )
 
     random.shuffle(password)
     my_password = "".join(password)
@@ -40,18 +43,22 @@ def password_gen():
 
 
 @app.get("/", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
-def index(request: Request):
-    return templates.TemplateResponse(request=request,
-                                      name="index.html")
+def index(request: Request) -> Any:
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @app.post("/gen", response_class=HTMLResponse, status_code=status.HTTP_201_CREATED)
-def gen_pass(request: Request, data: Annotated[FormData, Form()]):
-    my_password = password_gen()
+def gen_pass(request: Request, data: Annotated[FormData, Form()]) -> Any:
+    username = data.username
+    password = data.password
 
-    return templates.TemplateResponse(request=request,
-                                      name="index.html",
-                                      context={"my_password": my_password})
+    if username == "admin" and password == "admin":
+        my_password = password_gen()
+        return templates.TemplateResponse(
+            request=request, name="index.html", context={"my_password": my_password}
+        )
+    else:
+        raise HTTPException(401, "Invalid username or password")
 
 
 if __name__ == "__main__":
